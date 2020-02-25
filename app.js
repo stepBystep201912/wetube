250848ecad176f44993dd330ed06c6782d402821
stepBystep201912 <58474757+stepBystep201912@users.noreply.github.com>
2020-02-25 23:04:36 +0900

    200225 study

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
+
+app.use(helmet());
 
 app.set("view engine", "pug");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 // app.use(betweenHome); //모든 함수에 미들웨어로 사용
 // app.get("/", betweenHome, handleHome); //이 라우터에서만 사용하는 미들웨어
+
+app.use(localsMiddleware);
 
 // use() 사용한 거 주목 /
 // /user에 접속했을 때에 userRouter가 작동되는 거라서  /user 뒤에 붙는 형식이에요!
